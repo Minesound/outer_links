da5b51dfa9d4714e60f546f3361c86700cdc27c7
Cristhian Melo <rxtsel@hotmail.com>
2022-08-23 00:31:55 -0500

    fix: remove error loading css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://rxtsel.github.io",
-  base: "/free-premium-programs"
+  base: "/"
 });
