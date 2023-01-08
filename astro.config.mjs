5f01354ff0892189798f423a939f355943685cc4
Cristhian Melo <rxtsel@hotmail.com>
2023-01-08 09:37:39 -0500

    chore(dep): add tailwind, react and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rxtsel.github.io",
-  base: "/"
+  base: "/",
+  integrations: [react(), image(), sitemap(), tailwind()]
 });
